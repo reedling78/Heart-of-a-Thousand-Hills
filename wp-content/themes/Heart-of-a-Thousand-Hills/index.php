b552b638b8d1de7bcc77a39a974bf3db3bf52647
Ashley <ansulli12@gmail.com>
2015-05-02 18:43:38 -0500

    fixing merge conflict
    fixed merge conflict

--- wp-content/themes/Heart-of-a-Thousand-Hills/index.php
+++ wp-content/themes/Heart-of-a-Thousand-Hills/index.php
@@ -10,40 +10,25 @@
 
 
 <!-- CONTENT AREA 1 - IN BETA -->
-<<<<<<< Updated upstream
-<div class="video-bg">
-	<video autoplay="" loop="" style="visibility: visible; margin: auto; position: absolute; z-index: -1; top: 50%; left: 50%; -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%); width: 100%; height: auto;">
-		<source src="http://localhost:8888/Heart-of-a-Thousand-Hills/wp-content/uploads/2015/05/HighFives-min.mp4" type="video/mp4">
-		<source src="../video/background.webm" type="video/webm">
-		<source src="../video/background.ogv" type="video/ogg">
-	</video>
-</div>
 
-<div class="container">
-	<div class="row">
-=======
 <div class="row home">
 	<div class="column large-12">
-<<<<<<< HEAD
-		<video autoplay="" loop="" style="visibility: visible; margin: auto; position: absolute; z-index: -1; top: 50%; left: 50%; -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%);">
+<video autoplay="" loop="" style="visibility: visible; margin: auto; position: absolute; z-index: -1; top: 50%; left: 50%; -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%);">
 			<source src="http://localhost:8888/Heart-of-a-Thousand-Hills/wp-content/uploads/2015/05/HighFives-min.mp4" type="video/mp4">
 			<source src="http://localhost:8888/Heart-of-a-Thousand-Hills/wp-content/uploads/2015/05/HighFives-min.webm" type="video/webm">
 			<source src="http://localhost:8888/Heart-of-a-Thousand-Hills/wp-content/uploads/2015/05/HighFives-min.ogg" type="video/ogg">
-=======
-		<video autoplay="" loop="" style="visibility: visible; margin: auto; position: absolute; z-index: -1; top: 50%; left: 50%; -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%); width: 100%; min-height: 900px;">
-			<source src="<?=IMAGES?>/HighFives-min.mp4" type="video/mp4">
-			<source src="<?=IMAGES?>/HighFives-min.webm" type="video/webm">
-			<source src="<?=IMAGES?>/HighFives-min.ogg" type="video/ogg">
->>>>>>> origin/master
 		</video>
 		<h2>- Heart of A Thousand Hills -</h2>
->>>>>>> Stashed changes
 		<h1>The children of Rwanda</h1>
 		<p>Bringing school supplies, uniforms and the hope of a better tomorrow.</p>
 	</div>
 </div>
 
 <!-- END CONTENT AREA 1 -->
+        
+        <!-- CONTENT AREA 3 - IN BETA -->
+        
+        <!-- END CONTENT AREA 3 -->
 
 
 <?php get_footer(); ?>
