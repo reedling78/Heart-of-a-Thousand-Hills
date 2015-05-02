628ac2d4d44fa02a66b6657c2a49ed860bb10e43
Brandon Nicoll <bgnicoll@gmail.com>
2015-05-02 16:32:32 -0500

    Change to allow SVGs to be uploaded. Maybe

--- wp-content/themes/Heart-of-a-Thousand-Hills/functions.php
+++ wp-content/themes/Heart-of-a-Thousand-Hills/functions.php
@@ -285,4 +285,11 @@ if ( ! function_exists( 'alpha_scripts' ) ) {
 
 	add_action( 'wp_enqueue_scripts', 'alpha_scripts' );
 }
+if ( ! function_exists( 'cc_mime_types' ) ) {
+	function cc_mime_types($mimes) {
+	  $mimes['svg'] = 'image/svg+xml';
+	  return $mimes;
+	}
+	add_filter('upload_mimes', 'cc_mime_types');
+}
 ?>
