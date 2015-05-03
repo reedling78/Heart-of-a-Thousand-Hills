99b9c8e4276912f600805804251d809c85273588
Ashley <ansulli12@gmail.com>
2015-05-02 23:44:34 -0500

    foot work

--- wp-content/themes/Heart-of-a-Thousand-Hills/footer.php
+++ wp-content/themes/Heart-of-a-Thousand-Hills/footer.php
@@ -13,14 +13,20 @@
 	<footer class="site-footer">
 		<div class="container">
 			<?php get_sidebar( 'footer' ); ?>
-			
-			<div class="copyright">
-				<p>
-					&copy; <?php echo date( 'Y' ); ?>
-					<a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a>
-					<?php _e( 'All rights reserved.', 'alpha' ); ?>
-				</p>
-			</div> <!-- end copyright -->
+			 <div class="column large-12 footer">
+                 <ul class="inline-list">
+                    <li><a href="https://www.facebook.com/heartofathousandhills" target="_blank"><img src="<?= IMAGES ?>/facebook.svg" alt="facebook" /></a></li>
+                    <li><a href="https://twitter.com/HeartofHills" target="_blank"><img src="<?= IMAGES ?>/twitter.svg" alt="twitter" /></a></li>
+                    <li><a href="https://instagram.com/heartofthehills/" target="_blank"><img src="<?= IMAGES ?>/instagram.svg" alt="instagram" /></a></li>
+                </ul>
+                <div class="copyright">
+                    <p>
+                        &copy; <?php echo date( 'Y' ); ?>
+                        <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a>
+                        <?php _e( 'All rights reserved.', 'alpha' ); ?>
+                    </p>
+                </div> <!-- end copyright -->
+            </div> <!-- end footer row -->
 		</div> <!-- end container -->
 	</footer> <!-- end site-footer -->
 
