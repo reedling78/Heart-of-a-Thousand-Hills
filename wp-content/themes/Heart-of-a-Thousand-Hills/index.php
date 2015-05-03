8bc5262923dde4707ea0d817ed398b3f953c30fc
Venkat Yetrintala <venkat@fonts.com>
2015-05-03 02:42:30 -0500

    Modal

--- wp-content/themes/Heart-of-a-Thousand-Hills/index.php
+++ wp-content/themes/Heart-of-a-Thousand-Hills/index.php
@@ -45,13 +45,13 @@
 		<h3>- Donate Money -</h3>
 		<h2>Your Money</h2>
 		<p>Buys supplies.</p>
-		<p><a href="#" class="button">Donate Now</a></p>
+		<p><a href="#" class="button" data-reveal-id="myModal">Donate Now</a></p>
 	</div>
 	<div class="column large-6 keylime">
 		<h3>- Donate Time -</h3>
 		<h2>Your Time</h2>
 		<p>Makes a huge difference.</p>
-		<p><a href="#" class="button">Sign Me Up</a></p>
+		<p><a href="#" class="button" data-reveal-id="myModal">Sign Me Up</a></p>
 	</div>
 	<a href="#" class="event-button seaweed">
 		<div><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="32px" height="32px" viewBox="1.6 -3.2 32.0 43.2" enable-background="new 0 0 32 32" xml:space="preserve"><g><path fill="#000000" d="M29.334 3H25V1c0-0.553-0.447-1-1-1s-1 0.447-1 1v2h-6V1c0-0.553-0.448-1-1-1s-1 0.447-1 1v2H9V1 c0-0.553-0.448-1-1-1S7 0.4 7 1v2H2.667C1.194 3 0 4.2 0 5.666v23.667C0 30.8 1.2 32 2.7 32h26.667 C30.807 32 32 30.8 32 29.333V5.666C32 4.2 30.8 3 29.3 3z M30 29.333C30 29.7 29.7 30 29.3 30H2.667 C2.299 30 2 29.7 2 29.333V5.666C2 5.3 2.3 5 2.7 5H7v2c0 0.6 0.4 1 1 1s1-0.447 1-1V5h6v2c0 0.6 0.4 1 1 1 s1-0.447 1-1V5h6v2c0 0.6 0.4 1 1 1s1-0.447 1-1V5h4.334C29.701 5 30 5.3 30 5.666V29.333z"/><rect x="7" y="12" fill="#000000" width="4" height="3"/><rect x="7" y="17" fill="#000000" width="4" height="3"/><rect x="7" y="22" fill="#000000" width="4" height="3"/><rect x="14" y="22" fill="#000000" width="4" height="3"/><rect x="14" y="17" fill="#000000" width="4" height="3"/><rect x="14" y="12" fill="#000000" width="4" height="3"/><rect x="21" y="22" fill="#000000" width="4" height="3"/><rect x="21" y="17" fill="#000000" width="4" height="3"/><rect x="21" y="12" fill="#000000" width="4" height="3"/></g></svg></div>
