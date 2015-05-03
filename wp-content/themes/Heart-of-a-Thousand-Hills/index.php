a0fa2412378398fdc0657f1ed6d6f1bb65a7c205
Reed Rizzo <reedling78@gmail.com>
2015-05-03 04:31:03 -0500

    lkhjadgsfhjkgadsfbhjl

--- wp-content/themes/Heart-of-a-Thousand-Hills/index.php
+++ wp-content/themes/Heart-of-a-Thousand-Hills/index.php
@@ -60,7 +60,9 @@
 <?php require_once('content-blog.php'); ?>
 <!-- END CONTENT AREA 6 -->
 
-<?php get_post( 2 ); ?> 
+<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+<?php endif; ?>
 
 
 <?php get_footer(); ?>
