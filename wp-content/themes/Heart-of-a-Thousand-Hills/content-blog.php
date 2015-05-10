46f0deb4e806e80de1c3b3a1758cee19bc78d0d5
Brandon Nicoll <bgnicoll@gmail.com>
2015-05-10 07:19:32 -0500

    Fixed two bugs. One assumed we would always have more posts on initial page load, the other assumed we would always have posts for the current year on page load

--- wp-content/themes/Heart-of-a-Thousand-Hills/content-blog.php
+++ wp-content/themes/Heart-of-a-Thousand-Hills/content-blog.php
@@ -10,9 +10,16 @@ query_posts($args); ?>
 				<?php
 				$currentYear = date("Y");
 				$years = $wpdb->get_col("SELECT DISTINCT YEAR(post_date) FROM $wpdb->posts ORDER BY post_date DESC");
+				$defaultYear = true;
 				foreach($years as $year) : ?>
 				<?php 
-				$selectedClass = $currentYear == $year ? 'selected' : ''; ?>
+				$selectedClass = $defaultYear ? 'selected' : ''; 
+				if ($defaultYear){
+					$defaultYear = false;
+					$currentYear = $year;
+				}
+				 ?>
+
 				<li><a href="#" data-post-key="year" data-current-page="1" data-requested-year="<?php echo $year; ?>" class="blog-page <?php echo $selectedClass; ?>"><?php echo $year; ?></a></li>
 				<?php endforeach; ?>
 				
@@ -21,7 +28,23 @@ query_posts($args); ?>
 		<div class="columns small-4">
 			<ul class="inline-list arrows">
 				<li><span class="blog-page disabled" data-requested-year="<?php echo $currentYear; ?>" data-current-page="1" data-post-key="previous"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 73.7 74.2" enable-background="new 0 0 73.7 74.2" xml:space="preserve"><g><path fill="#C0C0AF" d="M73.1 40.6H13.6L42 69l-5 5L0 37l37-37l5 5L13.6 33.5h59.5V40.6z"/></g></svg></span></li>
+				<?php 
+				$argsNext= array(
+				'posts_per_page' => 6, 
+				'paged' => 2,
+				'year' => $currentYear
+				);
+				$postsNext = get_posts($argsNext);
+				$nextCount = count($postsNext);
+				$nextEnabled = $nextCount > 0;
+
+				?>
+				<?php if ($nextEnabled) : ?>
 				<li><a href="#" class="blog-page" data-requested-year="<?php echo $currentYear; ?>" data-current-page="1" data-post-key="next"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 73.7 74.2" enable-background="new 0 0 73.7 74.2" xml:space="preserve"><g><path fill="#C0C0AF" d="M73.3 37l-37 37l-5-5l28.4-28.4H0.3v-7.1h59.5L31.3 5.1l5-5L73.3 37z"/></g></svg></a></li>
+				<?php else: ?>
+				<li><span class="blog-page" data-requested-year="<?php echo $currentYear; ?>" data-current-page="1" data-post-key="next"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 73.7 74.2" enable-background="new 0 0 73.7 74.2" xml:space="preserve"><g><path fill="#C0C0AF" d="M73.3 37l-37 37l-5-5l28.4-28.4H0.3v-7.1h59.5L31.3 5.1l5-5L73.3 37z"/></g></svg></span></li>
+				<?php endif; ?>
+				
 			</ul>
 		</div>
 	</div>
